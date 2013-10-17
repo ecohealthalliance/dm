45c1d772ae5b08cd54e5bc2cb556d57ac61136a1
nicopresto <nicopresto@gmail.com>
2013-10-16 20:30:21 -0400

    removed stub to sort entries

--- server/publications.js
+++ server/publications.js
@@ -6,14 +6,6 @@ Meteor.publish('topEntries', function(limit) {
   return Entries.find({}, {sort: {votes: -1, submitted: -1}, limit: limit});
 });
 
-Meteor.publish('comments', function(entryId) {
-  return Comments.find({entryId: entryId});
-});
-
-Meteor.publish('notifications', function() {
-  return Notifications.find({userId: this.userId});
-});
-
 Meteor.publish('newEntries', function(limit) {
   return Entries.find({}, {sort: {submitted: -1}, limit: limit});
 });
@@ -22,11 +14,18 @@ Meteor.publish('singleEntry', function(id) {
   return id && Entries.find(id);
 });
 
+Meteor.publish('comments', function(entryId) {
+  return Comments.find({entryId: entryId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId});
+});
+
 Meteor.publish('questions', function(limit) {
   return Questions.find({}, {sort: {questionNumber: 1}, limit: limit});
 });
 
-/* fix this in entry */
 Meteor.publish('newQuestions', function(limit) {
   return Questions.find({}, {sort: {questionNumber: 1}, limit: limit});
 });
