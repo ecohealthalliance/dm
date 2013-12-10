9afc35ba665d3a608694cc100ef0e62b150719ca
Nico Preston <nicopresto@gmail.com>
2013-12-10 14:43:23 -0500

    fixes #26 added views of data when review is selected

--- client/views/entries/entry_page.js
+++ client/views/entries/entry_page.js
@@ -1,8 +1,15 @@
 Template.entryPage.helpers({
-  currentEntry: function() {
+  currententry: function() {
     return Entries.findOne(Session.get('currentEntryId'));
+  },
+  /*todo: combine questions and values from entry*/
+  questions: function() {
+    return Questions.find();
+  },
+  answer: function(questionTitle) {
+    return Entries.findOne(Session.get('currentEntryId'))[questionTitle]
   },
   comments: function() {
     return Comments.find({entryId: this._id});
-  }
+  },
 });
